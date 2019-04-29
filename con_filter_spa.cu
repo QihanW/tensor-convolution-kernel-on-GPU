15a2b908dda259e7672948d226b5135e850a9e61
QihanW <32336815+QihanW@users.noreply.github.com>
2019-04-28 23:45:44 -0400

    Update con_filter_spa.cu

--- con_filter_spa.cu
+++ con_filter_spa.cu
@@ -12,17 +12,17 @@
 
 int compute(float *img, float *f, float * out, int bh, int bw, int imgH, int imgW, int imgN, int nF, int convH, int convW) {
 
-    int con, imgg, inm1, inm2, inm3, inm4, inf, ind1, ind2, ind3;
+    int inm1, inm2, inm3, inm4, inf, ind1, ind2, ind3;
     inm1 = 0;
     inf = 0;
     ind1 = 0;
 
-    for (int mi = 0; i < imgN; i++){
+    for (int mi = 0; mi < imgN; mi++){
         ind1 += convW * convH;
         inm1 += imgW * imgH;
         for (int mj = 0; mj < convH; mj++){
             ind2 = ind1 + convW * mj;
-            inm2 = inm2 + imgW * mj;
+            inm2 = inm1 + imgW * mj;
             for (int mk = 0; mk < convW; mk++){
                 ind3 = ind2 + mk;
                 inm3 = inm2 + mk;
@@ -30,28 +30,29 @@ int compute(float *img, float *f, float * out, int bh, int bw, int imgH, int img
                     inm4 = inm3 + imgW * fi;
                     inf = ind3*nF*nF + fi*nF;
                     for (int fj = 0; fj < nF; fj++){
-                        out[ind] += img[inm4+fj] * f[inf+fj];
+                        out[ind3] += img[inm4+fj] * f[inf+fj];
                     }
                 }
             }
         }
     }
+    return 0;
 }
 
 int compute_csr(float *img, float *f_csr, int *pos, int *coor, float * out, int bh, int bw, int imgH, int imgW, int imgN, int nF, int convH, int convW) {
 
-    int con, imgg, inm1, inm2, inm3, inm4, inf, inf1, inf2, ind1, ind2, ind3;
+    int inm1, inm2, inm3, inm4, inf, inf1, inf2, ind1, ind2, ind3;
 
-    for (int mi = 0; i < imgN; i++){
+    for (int mi = 0; mi < imgN; mi++){
         ind1 = mi * convW * convH;
         inm1 = mi * imgW * imgH;
         for (int mj = 0; mj < convH; mj++){
             ind2 = ind1 + convW * mj;
-            inm2 = inm2 + imgW * mj;
+            inm2 = inm1 + imgW * mj;
             for (int mk = 0; mk < convW; mk++){
                 ind3 = ind2 + mk;
                 inm3 = inm2 + mk;
-                inf = nf*nf*ind3;
+                inf = nF*nF*ind3;
                 for (int fi = 0; fi < nF; fi++){
                     inf1 = pos[fi+inf];
                     inf2 = pos[fi+1+inf];
@@ -64,27 +65,27 @@ int compute_csr(float *img, float *f_csr, int *pos, int *coor, float * out, int 
             }
         }
     }
-    
+    return 0;
 }
 
 
 
-__global__ void compute_gpu_csr(float *img, float *out, int bh, int bw, int imgH, int imgW, int imgN, int nF, int convH, int convW){
+__global__ void compute_gpu_csr(float *img, float *f_csr, int *pos, int *coor, float *out, int bh, int bw, int imgH, int imgW, int imgN, int nF, int convH, int convW){
     
     int idX = blockDim.x * blockIdx.x + threadIdx.x;
     int idY = blockDim.y * blockIdx.y + threadIdx.y;
     
     int con, imgg, inm1, inm2, inm3, inm4, inf, inf1, inf2, ind1, ind2, ind3;
 
-    for (int mi = 0; i < imgN; i++){
+    for (int mi = 0; mi < imgN; mi++){
         ind1 = mi * convW * convH;
         inm1 = mi * imgW * imgH;
         if (idX < convH && idY < convW){
             ind2 = ind1 + convW * idX;
-            inm2 = inm2 + imgW * idX;
+            inm2 = inm1 + imgW * idX;
             ind3 = ind2 + idY;
             inm3 = inm2 + idY;
-            inf = nf*nf*ind3;
+            inf = nF*nF*ind3;
             for (int fi = 0; fi < nF; fi++){
                 inf1 = pos[fi+inf];
                 inf2 = pos[fi+1+inf];
@@ -157,16 +158,15 @@ int main(int argc, char **argv){
     float *h_fcsr = new float[num];
     int csrposSize = (k+1)*convDims*sizeof(int);
     int csrcooSize = num*sizeof(int);
-    for(int k=0; k<convDims; k++){
-        pos[0+k*convDims] = 0;
+    for(int ki=0; ki<convDims; ki++){
+        pos[0+ki*convDims] = 0;
         int index_p=0;
-        int index_c=0;
-        int z = k*convDims;
+        int z = ki*convDims;
         for(int i=0; i<nF; i++){
             for (int j=0; j<nF; j++){
-                if (h_img[i*nF+j+k] != 0){
+                if (filter[i*nF+j+z] != 0){
                     coor[index_p+k] = j;
-                    h_fcsr[index_p+k] = filter[i*nF+j+k];
+                    h_fcsr[index_p+k] = filter[i*nF+j+z];
                     index_p++;
                 }
             }
@@ -176,9 +176,9 @@ int main(int argc, char **argv){
 
 
     // create device array that can hold pixel intensity values in GPU GM
-    float *d_img_csr;
+    float *d_img;
     float *d_convolved;
-    float *d_filter
+    float *d_filter;
     int *d_pos;
     int *d_coor;
     cudaMemcpyToSymbol(d_filter, h_fcsr, filterSize);
@@ -210,8 +210,8 @@ int main(int argc, char **argv){
     printf("time: %f \n", elapsed);
     delete h_img;
     delete h_convolved;
-    delete pos;
-    delete h_imgcsr;
-    delete coor;
+    delete h_pos;
+    delete h_fcsr;
+    delete h_coor;
     return 0;
 }
